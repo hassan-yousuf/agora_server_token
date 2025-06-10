28c392f6714f9361df55cc340c9902c5e66f1fdd
Hassan Dev <yousufhassan648@gmail.com>
2025-06-10 17:59:49 +0500

    all fixed now

--- main.go
+++ main.go
@@ -13,10 +13,7 @@ import (
 )
 
 func main() {
-    err := godotenv.Load()
-    if err != nil {
-        panic("Error loading .env file")
-    }
+    _ = godotenv.Load()
 
     appID := os.Getenv("APP_ID")
     appCertificate := os.Getenv("APP_CERTIFICATE")
